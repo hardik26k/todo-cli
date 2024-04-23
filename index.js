c4030a6380ee4b2407ab194feb9d472f30edf719
hardik26k <hardik26k@gmail.com>
2024-04-23 15:54:05 +0530

    implemented a delete task function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {program} from 'commander'
 import list from './commands/list.js'
 import add from './commands/add.js'
 import markDone from './commands/markDone.js'
-
+import deleteTask from './commands/delete.js'
 
 
 program
@@ -22,6 +22,12 @@ program
     .description('Mark a task as completed')
     .option('-t or --task <tasks...> Specify the id of the task to marked as completed')
     .action(markDone)
+
+program
+    .command('delete')
+    .description('Remove a task from the list')
+    .option('-t or --task <task> Specify the id of the task to be deleted')
+    .action(deleteTask)
 
 
 program.parse()
